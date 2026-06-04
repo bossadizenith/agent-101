ea8d9fa6a230f3be2d0c4e1bf69126c72ff99684
bossadizenith <org.codeen@gmail.com>
2026-06-04 04:58:39 +0100

    feat: integrate @tavily/core for web search and file writing tools

--- index.ts
+++ index.ts
@@ -3,22 +3,31 @@ config();
 
 import { stdin as input, stdout as output } from "node:process";
 import * as readline from "node:readline/promises";
+import fs from "node:fs/promises";
 
 import { google } from "@ai-sdk/google";
 import { isLoopFinished, streamText, tool } from "ai";
 import { z } from "zod";
+import { tavily } from "@tavily/core";
+
+const TAVILY_API_KEY = process.env.TAVILY_API_KEY;
+
+const tvly = tavily({
+  apiKey: TAVILY_API_KEY,
+});
 
 const main = async () => {
   const rl = readline.createInterface({ input, output });
-  const prompt = await rl.question(
-    "Who's public repositories do you want to see?: ",
-  );
+  const prompt = await rl.question("What do you want to search for?: ");
 
   const result = streamText({
-    model: google("gemini-3.5-flash"),
-    prompt: `List the public repositories of ${prompt}`,
-    tools: { githubTool },
+    model: google("gemini-2.0-flash"),
+    prompt,
+    tools: { githubTool, webSearchTool, summary },
     stopWhen: isLoopFinished(),
+    onStepFinish: ({ usage, stepNumber, toolCalls }) => {
+      console.log(`Step ${stepNumber} finished: ${usage.totalTokens}`);
+    },
     experimental_onToolCallStart: ({ toolCall }) => {
       console.log(`Calling tool: ${toolCall.toolName}`);
     },
@@ -27,6 +36,9 @@ const main = async () => {
   for await (const textPart of result.textStream) {
     console.log(textPart);
   }
+
+  //   const steps = (await result.steps).flatMap((step) => step.toolCalls);
+  //   console.log({ steps });
 
   rl.close();
 };
@@ -42,6 +54,27 @@ const githubTool = tool({
     );
     const data = await response.json();
     return data;
+  },
+});
+
+const webSearchTool = tool({
+  description: "A tool for searching the web",
+  inputSchema: z.object({
+    query: z.string().describe("The query to search the web for"),
+  }),
+  execute: async ({ query }) => {
+    return await tvly.search(query);
+  },
+});
+
+const summary = tool({
+  description: "A tool for writing a file",
+  inputSchema: z.object({
+    path: z.string().describe("The path to the file to write"),
+    content: z.string().describe("The content to write to the file"),
+  }),
+  execute: async ({ path, content }) => {
+    await fs.writeFile(path, content);
   },
 });
 
