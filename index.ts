d40fea664401f30dd37859db6a6276607d8555d4
bossadizenith <org.codeen@gmail.com>
2026-06-05 22:08:49 +0100

    refactor: remove unused AI imports to simplify code and improve clarity

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import "dotenv/config";
 
-import { langchainAi } from "./ai/langchain-ai";
 import { search } from "./tools/search";
-import { vercelAi } from "./ai/vercel-ai";
 
 search("John Doe");
