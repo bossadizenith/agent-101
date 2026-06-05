e6948ad741104fc6ede3a6080b2a8b4bde8b5b79
bossadizenith <org.codeen@gmail.com>
2026-06-05 03:57:38 +0100

    refactor: replace main function with direct langchainAi invocation, simplifying entry point

--- index.ts
+++ index.ts
@@ -1,37 +1,3 @@
-import { config } from "dotenv";
-config();
+import { langchainAi } from "./ai/langchain-ai";
 
-import { stdin as input, stdout as output } from "node:process";
-import * as readline from "node:readline/promises";
-
-import { google } from "@ai-sdk/google";
-import { isLoopFinished, streamText } from "ai";
-import { SYSTEM_PROMPT } from "./lib/const";
-import { tools } from "./tools";
-
-const main = async () => {
-  const rl = readline.createInterface({ input, output });
-  const prompt = await rl.question("What do you want to search for?: ");
-
-  const result = streamText({
-    model: google("gemini-2.0-flash"),
-    system: SYSTEM_PROMPT,
-    prompt,
-    tools,
-    stopWhen: isLoopFinished(),
-    // onStepFinish: ({ usage, stepNumber, toolCalls }) => {
-    //   console.log(`Step ${stepNumber} finished: ${usage.totalTokens}`);
-    // },
-    experimental_onToolCallStart: ({ toolCall }) => {
-      console.log(`Calling tool: ${toolCall.toolName}`);
-    },
-  });
-
-  for await (const textPart of result.textStream) {
-    console.log(textPart);
-  }
-
-  rl.close();
-};
-
-main();
+langchainAi();
