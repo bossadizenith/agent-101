aefc85cab0548fbeece2374b43ea6c4c36b05c4b
bossadizenith <org.codeen@gmail.com>
2026-06-06 15:10:21 +0100

    feat: add @ai-sdk/groq dependency and implement text generation functionality using groq model

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import "dotenv/config";
 
-import { search } from "./tools/search";
+import { groq } from "@ai-sdk/groq";
+import { generateText } from "ai";
+import { tools } from "./tools";
 
-search("John Doe");
+const result = await generateText({
+  model: groq("llama-3.3-70b-versatile"),
+  prompt:
+    "who is bossadizenith, what are his github repos and produce me a report",
+  tools,
+});
+
+console.log(result.text);
