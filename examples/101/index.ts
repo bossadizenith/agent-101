9ab16a80ea0ff8483be49e9a168a1015da713bc3
bossadizenith <org.codeen@gmail.com>
2026-06-20 16:28:40 +0100

    chore(example): update demo query for weather use case

--- examples/101/index.ts
+++ examples/101/index.ts
@@ -2,8 +2,7 @@ import { runAgent } from "./lib/agent";
 import { DEFAULT_MODEL_ID } from "./lib/const";
 import { runtime } from "./lib/runtime/instance";
 
-export const query =
-  "who's bossadi zenith, github repos and gimme a report of what you found";
+export const query = "what's the weather in tokyo?";
 
 function getReplayRunId(): string | undefined {
   const flagIndex = process.argv.indexOf("--replay");
